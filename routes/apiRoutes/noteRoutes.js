00bdd7381d293d1cb3957145af5fa2c7928b510c
Todd Stuke <tddstuke@gmail.com>
2022-05-27 19:33:35 -0500

    added get route for notes and updated db.json to json format

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
 const { notes } = require("../../db/db.json");
-
-router.get("/notes"),
-  (req, res) => {
-    let results = notes;
-    console.log(results);
-    res.json(results);
-  };
+console.log(notes);
+router.get("/notes", (req, res) => {
+  let results = notes;
+  console.log(results);
+  res.json(results);
+});
 
 module.exports = router;
