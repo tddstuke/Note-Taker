13816a1884bc5bbbbe5b75ae327ba28cdb7531b8
Todd Stuke <tddstuke@gmail.com>
2022-05-27 19:34:08 -0500

    forgot to save before commit

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { notes } = require("../../db/db.json");
-console.log(notes);
+
 router.get("/notes", (req, res) => {
   let results = notes;
   console.log(results);
