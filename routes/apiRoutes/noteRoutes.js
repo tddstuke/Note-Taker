346533a3479c1b777e7e7b81223d551675e53725
Todd Stuke <tddstuke@gmail.com>
2022-05-28 14:18:23 -0500

    added POST route and createNote function

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,10 +1,20 @@
 const router = require("express").Router();
+const { append, json } = require("express/lib/response");
 const { notes } = require("../../db/db.json");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const createNewNote = require("../../lib/note");
 
 router.get("/notes", (req, res) => {
   let results = notes;
   console.log(results);
   res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+  console.log(req.body);
+  const note = createNewNote(req.body, notes);
+  res.json(req.body);
 });
 
 module.exports = router;
